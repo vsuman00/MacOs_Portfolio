5600f279aa3e0279f51010e8a454b29729a25d1b
VAIBHAV SUMAN <76209891+vsuman00@users.noreply.github.com>
2025-12-03 16:10:41 +0530

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import {Navbar, Welcome} from "#components"
+import { Navbar, Welcome, Dock } from "#components";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Welcome />
+      <Dock />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
